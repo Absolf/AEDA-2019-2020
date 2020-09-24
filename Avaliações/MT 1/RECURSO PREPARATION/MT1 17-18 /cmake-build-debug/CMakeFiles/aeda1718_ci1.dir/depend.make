3836579d76acfde5cb987ef86423f0b147d12cf1
Absolf <victorlaureanocg@gmail.com>
2020-09-24 14:54:24 +0100

    sorting folders

--- Avaliações/MT 1/RECURSO PREPARATION/MT1 17-18 /cmake-build-debug/CMakeFiles/aeda1718_ci1.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for aeda1718_ci1.
-# This may be replaced when dependencies are built.
--- Avaliações/MT 2/RECURSO PREPARATION/aeda1415_ci3_googleTest/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/laureano/Documents/FEUP 2019-2020/AEDA/Avaliações/MT 2/RECURSO PREPARATION/aeda1415_ci3_googleTest/lib/googletest-master
-# Build directory: /home/laureano/Documents/FEUP 2019-2020/AEDA/Avaliações/MT 2/RECURSO PREPARATION/aeda1415_ci3_googleTest/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
